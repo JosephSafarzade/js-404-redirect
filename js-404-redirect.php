adf301e6629b52541beeb8e20982f81819aaae81
Joseph Safarzade <jo.safarzade@gmail.com>
2022-07-17 14:44:44 +0430

    First Commit
    Added : main entry plugin file
    Added : initial folder structure

---
+++ js-404-redirect.php
@@ -0,0 +1,10 @@
+/*
+Plugin Name: JS 404 Redirect
+Plugin URI: https://safarzade.com
+Description: See and manage all of your website 404 errors and add redirection for each of them
+Version: 1.0
+Author: Joseph Safarzade
+License: GPLv3 or later
+Text Domain: js-404-redirect
+Domain Path: /languages
+*/
